699bace14a2f2a149ae18065100f70c99f7b46e5
Alex <cigan@ua.fm>
2021-01-25 16:11:12 +0200

    Use .reduce() and .concat() methods to get length of trasformedIngredients

--- burger_app/src/components/Burger/Burger.js
+++ burger_app/src/components/Burger/Burger.js
@@ -5,16 +5,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
 
-    const trasformedIngredients = Object.keys(props.ingredients).map(ingrKey => {   // перетворюємо об'єкт 'ingredients' на строку
+    let trasformedIngredients = Object.keys(props.ingredients).map(ingrKey => {   // перетворюємо об'єкт 'ingredients' на строку
         return [...Array(props.ingredients[ingrKey])].map((_, index) => {   // отримуємо масив, кількість елементів (інгрідієнтів) залежить від кількості, що вказана в state BurgerBuilder
             return <BurgerIngredient key={ingrKey + index} type={ingrKey} />       // ingrKey = state BurgerBuilder = keys switch.case of BurgerIngredient
         });
-    });
+    })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);  //  перетворюємо на масив щоб дізнатися довжину
+    // console.log(trasformedIngredients);
 
+    if (trasformedIngredients.length === 0) {
+        trasformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={styles.Burger}>
             <BurgerIngredient type="bread-top" />
+
             {trasformedIngredients}
+
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
