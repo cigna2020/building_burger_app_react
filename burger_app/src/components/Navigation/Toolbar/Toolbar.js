624392f88f54918920a4406ebe970d4b49054b80
Alex <cigan@ua.fm>
2021-01-26 19:33:28 +0200

    Create a NavItems component with links

--- burger_app/src/components/Navigation/Toolbar/Toolbar.js
+++ burger_app/src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 import styles from './Toolbar.module.css'
 import Logo from '../../Layout/Logo/Logo';
+import NavItems from '../NavItems/NavItems';
 
 const toolbar = (props) => (
     <header className={styles.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ...
+            <NavItems />
         </nav>
     </header>
 );
