c6c988cd8eb1bd42e5a8496daf5662a95da0e4e5
Alex <cigan@ua.fm>
2021-01-27 08:03:48 +0200

    Add Backdrop into SideDrawer and add a method into Layout to hide Backdrop and SideDrawer

--- burger_app/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger_app/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,17 +3,27 @@ import React from 'react';
 import Logo from '../../Layout/Logo/Logo';
 import NavItems from '../NavItems/NavItems';
 import styles from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [styles.SideDrawer, styles.Close]
+
+    if (props.open) {
+        attachedClasses = [styles.SideDrawer, styles.Open]
+    }
 
     return (
-        <div className={styles.SideDrawer}>
-            {/* <Logo height={"11%"} /> */}
-            <div className={styles.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                {/* <Logo height={"11%"} /> */}
+                <div className={styles.Logo}>
+                    <Logo />
+                </div>
+                <nav><NavItems /></nav>
             </div>
-            <nav><NavItems /></nav>
-        </div>
+        </Aux>
     );
 };
 
