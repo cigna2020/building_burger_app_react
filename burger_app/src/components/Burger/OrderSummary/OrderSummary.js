94447b1bc3800cee9336eb34214ac946f00664db
Alex <cigan@ua.fm>
2021-01-27 11:32:18 +0200

    Add shouldComponentUpdate() into Modal to updating OrderSummary and Modal only when Modal is shown

--- burger_app/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger_app/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients).map(ingrKey => {
-        return (
-            <li key={ingrKey}><span style={{textTransform: 'capitalize'}}>{ingrKey}</span>: {props.ingredients[ingrKey]}</li>
-        )
-    });
+class OrderSummary extends Component {
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul >
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} Euro</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancel}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
-        </Aux>
-    );
+    // componentDidUpdate() {
+    //     console.log('[OrderSummary] DidUpdate')  // Не обновляється, так як в обгортці (Модал) використано shouldComponentUpdate
+    // }
+
+    render() {
+
+        const ingredientsSummary = Object.keys(this.props.ingredients).map(ingrKey => {
+            return (
+                <li key={ingrKey}><span style={{textTransform: 'capitalize'}}>{ingrKey}</span>: {this.props.ingredients[ingrKey]}</li>
+            )
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul >
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)} Euro</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancel}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
 
-export default orderSummary;
+
+export default OrderSummary;
