3cf3122218c56e03d330dc9063a4da6ec4a5ea77
Alex <cigan@ua.fm>
2021-02-03 20:28:52 +0200

    Add Route-s to App.js to show BurgerBuilder and Checkout on a separate path

--- burger_app/src/App.js
+++ burger_app/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Switch, Route} from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -20,8 +21,10 @@ class App extends Component {
             <div>
                 <Layout>
                     {/* {this.state.show ? <BurgerBuilder /> : null} */}
-                    <BurgerBuilder />
-                    <Checkout />
+                    <Switch>
+                        <Route path="/checkout" component={Checkout} />
+                        <Route path="/" exact component={BurgerBuilder} />
+                    </Switch>
                 </Layout>
 
             </div>
