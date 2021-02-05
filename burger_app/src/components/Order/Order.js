31b9c5fa69406d86e0b61e7550ab57e4bebfe2b9
Alex <cigan@ua.fm>
2021-02-05 18:09:59 +0200

    Add methods to convert data from an object into text and render them

--- burger_app/src/components/Order/Order.js
+++ burger_app/src/components/Order/Order.js
@@ -2,11 +2,36 @@ import React from 'react-router-dom';
 
 import styles from './Order.module.css'
 
-const order = (props) => (
-    <div className={styles.Order}>
-        <p>Ingredients: Salad: (1)</p>
-        <p>Total price: <strong>Euro 5.55</strong></p>
-    </div>
-);
+const order = (props) => {
+
+    const ingredients = [];
+    for (let ingrName in props.ingredients) {
+        ingredients.push({
+            name: ingrName,
+            amount: props.ingredients[ingrName],
+        });
+    }
+
+    const ingrOutput = ingredients.map(ingr => {
+        return <span
+            key={ingr.name}
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}>
+            {ingr.name}: {ingr.amount};
+        </span>
+    });
+
+    return (
+        <div className={styles.Order}>
+            <p>Ingredients: {ingrOutput}</p>
+            <p>Total price: <strong>Euro {props.price.toFixed(2)}</strong></p>
+        </div>
+    )
+};
 
 export default order;
